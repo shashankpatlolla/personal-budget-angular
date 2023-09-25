a33b0edc03fa43b3fc27221b4417716488bda560
shashankpatlolla <143342432+shashankpatlolla@users.noreply.github.com>
2023-09-25 00:36:11 -0400

    Changes for the exercise 2

--- personal-budget/src/app/data.service.ts
+++ personal-budget/src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators'; // Import the tap operator
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,23 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class DataService {
   private data: any[] = [];
   private dataSubject: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  private dataLoaded: boolean = false; // Track whether data is already loaded
+
   constructor(private http: HttpClient) { }
 
   fetchData() {
-    return this.http.get('http://localhost:3005/budget');
+    if (!this.dataLoaded) {
+      return this.http.get('http://localhost:3005/budget').pipe(
+        tap((res: any) => {
+          this.dataLoaded = true;
+          this.data = res.myBudget;
+          this.dataSubject.next(this.data);        })
+      );
+    } else {
+      return this.dataSubject.asObservable();
+    }
   }
+  // fetchData() {
+  //   return this.http.get('http://localhost:3005/budget');
+  // }
 }
